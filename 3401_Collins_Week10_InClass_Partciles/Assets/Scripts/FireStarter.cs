11049ec8aa39aa6e26dffc0911b3fe549a1b9b85
Sean Collins <sc679115@ohio.edu>
2019-03-25 21:17:51 -0400

    particles

---
+++ 3401_Collins_Week10_InClass_Partciles/Assets/Scripts/FireStarter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireStarter : MonoBehaviour
+{
+    public ParticleSystem FireParticles;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        //Functions like play, isPlaying,stop are all used for detecting animation
+        if (Input.GetKeyDown(KeyCode.Space));
+        {
+            FireParticles.Play();
+        }
+        //Self explainatory line of code
+        if (FireParticles.isPlaying)
+        {
+            FireParticles.Stop();
+        }
+        else
+        {
+            FireParticles.Play();
+        }
+    }
+}
+
+
